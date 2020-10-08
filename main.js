7a6bac38c4b5017f7942866f133d162881a38537
Mingyu Cui <mingyucu@usc.edu>
2020-10-07 20:18:30 -0700

    8. Computed Properties

--- main.js
+++ main.js
@@ -1,24 +1,36 @@
 const app = Vue.createApp({
     data() {
         return {
-            cart:0,
+            cart: 0,
             product: 'Socks',
             brand: 'Vue Mastery',
-            image: './assets/images/socks_blue.jpg',
-            inStock: false,
+            selectVariant: 0,
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
-              { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
-              { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
-            ]
+                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
+                { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0 },
+            ],
+            onSale: true
         }
     },
     methods: {
         addToCart() {
             this.cart += 1
         },
-        updateImage(variantImage) {
-            this.image = variantImage
+        updateVariant(index) {
+            this.selectVariant = index
+                // console.log(index)
+        }
+    },
+    computed: {
+        title() {
+            return this.brand + ' ' + this.product + (this.onSale ? ' is on sale' : '')
+        },
+        image() {
+            return this.variants[this.selectVariant].image
+        },
+        inStock() {
+            return this.variants[this.selectVariant].quantity
         }
     }
 })
